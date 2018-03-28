b12fa4f7117441896a8e50657157ce73055626d1
Ragland <jakearagland@gmail.com>
2018-03-28 15:04:02 -0500

    npm doc is horrible for twitter

--- keys.js
+++ keys.js
@@ -1,15 +1,15 @@
 
-console.log(process.env.SECRET_MESSAGE);
-// console.log('this is loaded');
 
-// exports.twitter = {
-//   consumer_key: process.env.TWITTER_CONSUMER_KEY,
-//   consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-//   access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
-//   access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
-// };
+console.log('this is loaded');
 
-// exports.spotify = {
-//   id: process.env.SPOTIFY_ID,
-//   secret: process.env.SPOTIFY_SECRET
-// };
+exports.twitter = {
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+};
+
+exports.spotify = {
+  id: process.env.SPOTIFY_ID,
+  secret: process.env.SPOTIFY_SECRET
+};
--- liri.js
+++ liri.js
@@ -1,3 +1,36 @@
 
-require("dotenv/config")
-require("./keys")
+require('dotenv').config()
+var Twitter = require('twitter')
+var Spotify = require('node-spotify-api');
+var omdb = require('omdb');
+var config = require("./keys")
+console.log(config)
+var myArg = process.argv[2];
+console.log(myArg);
+// Twitter call and response tweets
+var client = new Twitter(config.twitter);
+var params = {screen_name: 'jakearagland'};
+client.get('statuses/user', params, function(error, tweets, response) {
+  if (!error) {
+    console.log(tweets);
+  } else {
+    console.log(error);
+  }
+});
+
+
+
+
+
+
+
+//console.log(someVariable) // undefined
+
+// function Car(make) {
+//     this.make = config.key
+// }
+
+/*var tweet = new Car('honda')
+var tweet2 = new Car('toyota')
+console.log(tweet.make)
+console.log(tweet2.make)*/
